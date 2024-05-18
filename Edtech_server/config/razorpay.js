cf08b6290b5a65cae35c9e077b57824d905aa7b2
Ishan Pandey <123641070+ishann73@users.noreply.github.com>
2024-05-18 14:31:32 +0530

    Update razorpay.js

--- Edtech_server/config/razorpay.js
+++ Edtech_server/config/razorpay.js
@@ -2,8 +2,8 @@
 const Razorpay = require('razorpay');
 
 // Your Razorpay API key and secret
-const key_id = 'rzp_test_qv0O4IufUzaLbB';
-const key_secret = 'WckjpE0VZs7xBjnCuhjfN7SG';
+const key_id = process.env.key_id;
+const key_secret = process.env.key_secret;
 
 // Create and export a new instance of the Razorpay class
 const razorpay = new Razorpay({
