4532a52ce0a4627fea2196d4759a9f9c693ad99b
Aman Shankar singh <96378145+singhAman05@users.noreply.github.com>
2024-05-18 12:44:56 +0530

    adding utils files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
-
+export const ACCOUNT_TYPE = {
+    STUDENT: "Student",
+    INSTRUCTOR: "Instructor",
+    ADMIN: "Admin",
+  }
+  
+  export const COURSE_STATUS = {
+    DRAFT: "Draft",
+    PUBLISHED: "Published",
+  }
+  
